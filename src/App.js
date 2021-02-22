281ec0326a86dcb67fa54e41883a5301a86269f9
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-22 11:56:38 +0100

    Starting with the Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import './App.css';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Navbar />
     </div>
   );
 }
