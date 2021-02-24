a98418776a98e6dc506ec68e168e34e81b240c00
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-24 13:31:15 +0100

    Adding the start button and fixing some bugs

--- src/components/HereSection/HeroElements.js
+++ src/components/HereSection/HeroElements.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { MdArrowForward, MdKeyboardArrowRight } from 'react-icons/md';
 
 export const HeroContainer = styled.div`
     background: #0c0c0c;
@@ -9,6 +10,18 @@ export const HeroContainer = styled.div`
     height: 800px;
     position: relative;
     z-index: 1;
+
+    :before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0; 
+        bottom: 0;
+        background: linear-gradient(180deg, rgb(0,0,0,0.2) 0%, rgba(0,0,0,0.6) 100%), 
+            linear-gradient(180deg, rgba(0,0,0,0.2) 0%, transparent 100%);
+        z-index: 2;
+    }
 `
 
 export const HeroBg = styled.div`
@@ -77,5 +90,11 @@ export const HeroBtnWrapper = styled.div`
 `
 
 export const ArrowForward = styled(MdArrowForward)`
-    
+    margin-left: 8px;
+    font-size: 20px;
+`
+
+export const ArrowRight = styled(MdKeyboardArrowRight)`
+    margin-left: 8px;
+    font-size: 20px; 
 `
--- src/components/HereSection/index.js
+++ src/components/HereSection/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Video from '../../videos/video.mp4';
+import { Button } from '../Sidebar/ButtonElement';
 import { 
     HeroContainer, 
     HeroBg, 
-    VideoBg 
+    VideoBg, 
+    HeroContent, 
+    HeroH1, 
+    HeroP, 
+    HeroBtnWrapper,
+    ArrowForward,
+    ArrowRight 
 } from './HeroElements';
 
 
 const HeroSection = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+
     return (
         <HeroContainer id="home">
             <HeroBg>
@@ -18,7 +31,7 @@ const HeroSection = () => {
                 <HeroH1>Virtual Banking Made Easy</HeroH1>
                 <HeroP>Sign up for a new account today and receive $250 in credits towards your next payment.</HeroP>
                 <HeroBtnWrapper>
-                    <Button to="signup">
+                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover} primary="true" dar="true">
                         Get started {hover ? <ArrowForward /> : <ArrowRight />}
                     </Button>
                 </HeroBtnWrapper>
