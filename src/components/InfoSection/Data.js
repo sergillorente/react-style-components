0795b1d351d4acb1fe276cd69ab15d1a0be707f2
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-24 17:28:54 +0100

    Adding the different sections that compose the total app

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,16 +1,50 @@
 export const homeObjOne = {
     id: 'about',
     lightBg: false,
-    lightText: true,
+    lightText: false,
     lightTextDesc: true,
     topLine: 'Premium Bank',
     headline: 'Unlimited Transactions with zero fees',
     description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees',
     buttonLabel: 'Get Started',
-    imgStart: false, 
+    imgStart: true, 
     img: require('../../images/svg-1.svg'),
     alt: 'Car',
     dark: true,
     primary: true,
-    darktext: false
+    darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees',
+    buttonLabel: 'Learn More',
+    imgStart: true, 
+    img: require('../../images/svg-2.svg'),
+    alt: 'Car',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
+export const homeObjThree = {
+    id: 'about',
+    lightBg: false,
+    lightText: false,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees',
+    buttonLabel: 'Get Started',
+    imgStart: true, 
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
 }
